85c37e263a9a53295fbd74301f99653cb056df45
Sami Elmadani <sae53@uclive.ac.nz>
2023-09-29 20:08:06 +1300

    Attempt at dark mode. Added color palette and tried using Compose color managing

--- app/src/main/java/com/example/geoquest/ui/theme/Theme.kt
+++ app/src/main/java/com/example/geoquest/ui/theme/Theme.kt
@@ -1,10 +1,12 @@
 package com.example.geoquest.ui.theme
 
+import android.annotation.SuppressLint
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 
 private val DarkColorPalette = darkColors(
     primary = Purple200,
@@ -42,3 +44,47 @@ fun GeoQuestTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composab
         content = content
     )
 }
+
+// Testing dark mode theming
+
+
+//private val LightThemeColors = lightColors(
+//    primary = Blue600,
+//    primaryVariant = Blue400,
+//    onPrimary = Black2,
+//    secondary = Color.White,
+//    secondaryVariant = Teal300,
+//    onSecondary = Color.Black,
+//    error = RedErrorDark,
+//    onError = RedErrorLight,
+//    background = Grey1,
+//    onBackground = Color.Black,
+//    surface = Color.White,
+//    onSurface = Black2,
+//)
+
+//private val DarkThemeColors = darkColors(
+//    primary = Blue700,
+//    primaryVariant = Color.White,
+//    onPrimary = Color.White,
+//    secondary = Black1,
+//    onSecondary = Color.White,
+//    error = RedErrorLight,
+//    background = Color.Black,
+//    onBackground = Color.White,
+//    surface = Black1,
+//    onSurface = Color.White,
+//)
+
+
+//@Composable
+//fun AppTheme(
+//    darkTheme: Boolean,
+//    content: @Composable () -> Unit,
+//) {
+//    MaterialTheme(
+//        colors = if (darkTheme) DarkThemeColors else LightThemeColors,
+//    ){
+//        content()
+//    }
+//}
