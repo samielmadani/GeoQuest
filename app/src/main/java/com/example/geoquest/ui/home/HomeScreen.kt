b99b773f2b21b87118adb3eded8109cf753edb44
Sami Elmadani <sae53@uclive.ac.nz>
2023-10-01 16:14:03 +1300

    colors

--- app/src/main/java/com/example/geoquest/ui/home/HomeScreen.kt
+++ app/src/main/java/com/example/geoquest/ui/home/HomeScreen.kt
@@ -254,7 +254,7 @@ fun QuestCard(
         },
         modifier = Modifier
             .background(
-                if (isSelected) if (isSystemInDarkTheme()) Color.Black else Color.LightGray else if (isSystemInDarkTheme()) Color.Transparent else Color.White
+                if (isSelected) if (isSystemInDarkTheme()) Color.Black else Color.LightGray else Color.Transparent
             )
             .fillMaxWidth()
             .padding(dimensionResource(id = R.dimen.padding_medium))
