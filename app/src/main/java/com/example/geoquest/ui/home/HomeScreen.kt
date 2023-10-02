49ba0034d562abf06dce48148fba21d5aeb52e16
mwe50 <mwe50@uclive.ac.nz>
2023-10-02 17:22:50 +1300

    Changed the way it works

--- app/src/main/java/com/example/geoquest/ui/home/HomeScreen.kt
+++ app/src/main/java/com/example/geoquest/ui/home/HomeScreen.kt
@@ -2,10 +2,7 @@ package com.example.geoquest.ui.home
 
 import android.Manifest
 import android.content.Context
-import android.content.Intent
-import android.nfc.NdefMessage
-import android.nfc.NdefRecord
-import android.nfc.NfcAdapter
+import android.os.Build
 import android.util.Log
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
@@ -56,7 +53,6 @@ import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import coil.compose.rememberAsyncImagePainter
@@ -68,7 +64,6 @@ import com.example.geoquest.ui.navigation.NavigationDestination
 import com.example.geoquest.ui.quest.createQuest.CreateQuestViewModel
 import com.example.geoquest.ui.quest.findQuest.BackPressHandler
 import com.example.geoquest.ui.quest.viewQuest.DifficultyStars
-import com.example.geoquest.ui.theme.GeoQuestTheme
 import com.google.accompanist.permissions.ExperimentalPermissionsApi
 import com.google.accompanist.permissions.PermissionRequired
 import com.google.accompanist.permissions.PermissionState
@@ -81,6 +76,7 @@ import com.google.android.gms.nearby.connection.AdvertisingOptions
 import com.google.android.gms.nearby.connection.ConnectionInfo
 import com.google.android.gms.nearby.connection.ConnectionLifecycleCallback
 import com.google.android.gms.nearby.connection.ConnectionResolution
+import com.google.android.gms.nearby.connection.ConnectionsStatusCodes
 import com.google.android.gms.nearby.connection.DiscoveredEndpointInfo
 import com.google.android.gms.nearby.connection.DiscoveryOptions
 import com.google.android.gms.nearby.connection.EndpointDiscoveryCallback
@@ -88,6 +84,8 @@ import com.google.android.gms.nearby.connection.Payload
 import com.google.android.gms.nearby.connection.PayloadCallback
 import com.google.android.gms.nearby.connection.PayloadTransferUpdate
 import com.google.android.gms.nearby.connection.Strategy
+import com.google.android.gms.tasks.OnFailureListener
+import com.google.android.gms.tasks.OnSuccessListener
 import com.google.maps.android.compose.CameraPositionState
 import com.google.maps.android.compose.GoogleMap
 import com.google.maps.android.compose.MapProperties
@@ -96,6 +94,7 @@ import com.google.maps.android.compose.rememberCameraPositionState
 import com.google.maps.android.compose.rememberMarkerState
 import org.json.JSONObject
 import java.io.ByteArrayOutputStream
+
 
 object HomeDestination: NavigationDestination {
     override val route = "home"
@@ -432,122 +431,133 @@ fun convertQuestToJson(quest: Quest): String {
 //}
 
 fun discoverQuest(context: Context) {
-    Log.i("NFC-RECEIVE", "Called discover")
-    val connectionsClient = Nearby.getConnectionsClient(context)
+    startDiscovery(context)
+}
 
-    val discoveryOptions = DiscoveryOptions.Builder()
-        .setStrategy(Strategy.P2P_STAR)
-        .build()
+private fun startAdvertising(context: Context) {
+    val advertisingOptions = AdvertisingOptions.Builder().setStrategy(Strategy.P2P_STAR).build()
 
-    val payloadCallback = object : PayloadCallback() {
+    class ReceiveBytesPayloadListener : PayloadCallback() {
         override fun onPayloadReceived(endpointId: String, payload: Payload) {
-            // Handle received payloads here
-            val receivedQuestJson = String(payload.asBytes()!!)
-//            val quest = convertJsonToQuest(receivedQuestJson)
-            val data = JSONObject(receivedQuestJson)
-            Log.i("NFC-RECEIVE", "Received: $data")
-            // Now you have the received Quest object and can process it as needed
+            // This always gets the full data of the payload. Is null if it's not a BYTES payload.
+            if (payload.type == Payload.Type.BYTES) {
+                val receivedBytes = payload.asBytes()
+                Log.i("SHARE SEND", "Received data")
+            }
         }
 
         override fun onPayloadTransferUpdate(endpointId: String, update: PayloadTransferUpdate) {
-            // Handle payload transfer updates if needed
-            Log.i("NFC-RECEIVE", "Payload update? $update")
+            // Bytes payloads are sent as a single chunk, so you'll receive a SUCCESS update immediately
+            // after the call to onPayloadReceived().
+            Log.i("SHARE SEND", "Update requested??")
         }
     }
 
-    Log.i("NFC-RECEIVE", "Starting discovery")
-    connectionsClient.startDiscovery(
-        "com.example.geoquest", object : EndpointDiscoveryCallback() {
+    val connectionLifecycleCallback: ConnectionLifecycleCallback =
+        object : ConnectionLifecycleCallback() {
+            override fun onConnectionInitiated(endpointId: String, connectionInfo: ConnectionInfo) {
+                // Automatically accept the connection on both sides.
+                Log.i("SHARE SEND", "Connection initiated")
+                Nearby.getConnectionsClient(context).acceptConnection(endpointId, ReceiveBytesPayloadListener())
+            }
+
+            override fun onConnectionResult(endpointId: String, result: ConnectionResolution) {
+                Log.i("SHARE SEND", "Connection Result")
+                when (result.status.statusCode) {
+                    ConnectionsStatusCodes.STATUS_OK -> {}
+                    ConnectionsStatusCodes.STATUS_CONNECTION_REJECTED -> {}
+                    ConnectionsStatusCodes.STATUS_ERROR -> {}
+                    else -> {}
+                }
+            }
+
+            override fun onDisconnected(endpointId: String) {
+                // We've been disconnected from this endpoint. No more data can be
+                // sent or received.
+                Log.i("SHARE SEND", "Disconnected")
+            }
+        }
+
+    Nearby.getConnectionsClient(context)
+        .startAdvertising(
+            Build.ID, Build.MODEL, connectionLifecycleCallback, advertisingOptions
+        )
+        .addOnSuccessListener { Log.i("SHARE SEND", "startAdvertising OnSuccessListener") }
+        .addOnFailureListener { error -> Log.e("SHARE SEND", "startAdvertising Error: $error") }
+}
+
+private fun startDiscovery(context: Context) {
+    val discoveryOptions = DiscoveryOptions.Builder().setStrategy(Strategy.P2P_STAR).build()
+    val id = Build.ID
+    class ReceiveBytesPayloadListener : PayloadCallback() {
+        override fun onPayloadReceived(endpointId: String, payload: Payload) {
+            // This always gets the full data of the payload. Is null if it's not a BYTES payload.
+            if (payload.type == Payload.Type.BYTES) {
+                val receivedBytes = payload.asBytes()
+                Log.i("SHARE RCV", "Received data")
+            }
+        }
+
+        override fun onPayloadTransferUpdate(endpointId: String, update: PayloadTransferUpdate) {
+            // Bytes payloads are sent as a single chunk, so you'll receive a SUCCESS update immediately
+            // after the call to onPayloadReceived().
+            Log.i("SHARE RCV", "Update requested??")
+        }
+    }
+
+    val connectionLifecycleCallback: ConnectionLifecycleCallback =
+        object : ConnectionLifecycleCallback() {
+            override fun onConnectionInitiated(endpointId: String, connectionInfo: ConnectionInfo) {
+                // Automatically accept the connection on both sides.
+                Nearby.getConnectionsClient(context).acceptConnection(endpointId, ReceiveBytesPayloadListener())
+                Log.i("SHARE RCV", "Connection initiated")
+            }
+
+            override fun onConnectionResult(endpointId: String, result: ConnectionResolution) {
+                when (result.status.statusCode) {
+                    ConnectionsStatusCodes.STATUS_OK -> {}
+                    ConnectionsStatusCodes.STATUS_CONNECTION_REJECTED -> {}
+                    ConnectionsStatusCodes.STATUS_ERROR -> {}
+                    else -> {}
+                }
+
+                Log.i("SHARE RCV", "Connection result")
+            }
+
+            override fun onDisconnected(endpointId: String) {
+                // We've been disconnected from this endpoint. No more data can be
+                // sent or received.
+                Log.i("SHARE RCV", "Disconnected")
+            }
+        }
+
+    val endpointDiscoveryCallback: EndpointDiscoveryCallback =
+        object : EndpointDiscoveryCallback() {
             override fun onEndpointFound(endpointId: String, info: DiscoveredEndpointInfo) {
-                // An endpoint was found. Request a connection to it.
-                connectionsClient.requestConnection(
-                    "DeviceName",
-                    endpointId,
-                    object : ConnectionLifecycleCallback() {
-                        override fun onConnectionInitiated(endpointId: String, connectionInfo: ConnectionInfo) {
-                            // Automatically accept the connection
-                            connectionsClient.acceptConnection(endpointId, payloadCallback)
-                            Log.i("NFC-RECEIVE", "Connection accepted")
-                        }
+                Log.i("SHARE RCV", "Endpoint found")
 
-                        override fun onConnectionResult(endpointId: String, result: ConnectionResolution) {
-                            if (result.status.isSuccess) {
-                                // Connection was successful. You can now receive payloads.
-                                Log.i("NFC-RECEIVE", "Connection success")
-                            }
-                        }
-
-                        override fun onDisconnected(endpointId: String) {
-                            // Disconnected from the endpoint. Handle as needed.
-                            Log.i("NFC-RECEIVE", "Disconnected")
-                        }
-                    }
-                )
+                // An endpoint was found. We request a connection to it.
+                Nearby.getConnectionsClient(context)
+                    .requestConnection(id, endpointId, connectionLifecycleCallback)
+                    .addOnSuccessListener { Log.i("SHARE RCV", "requestConnection OnSuccessListener") }
+                    .addOnFailureListener { error -> Log.e("SHARE RCV", "requestConnection Error: $error") }
             }
 
             override fun onEndpointLost(endpointId: String) {
-                // A previously discovered endpoint has gone away. Handle as needed.
-                Log.i("NFC-RECEIVE", "Endpoint lost")
+                // A previously discovered endpoint has gone away.
+                Log.i("SHARE RCV", "Lost the endpoint")
             }
-        },
-        discoveryOptions
-    )
+        }
+
+    Nearby.getConnectionsClient(context)
+        .startDiscovery(id, endpointDiscoveryCallback, discoveryOptions)
+        .addOnSuccessListener { Log.i("SHARE RCV", "startDiscovery OnSuccessListener") }
+        .addOnFailureListener { error -> Log.e("SHARE RCV", "startDiscovery Error: $error") }
 }
 
 @OptIn(ExperimentalPermissionsApi::class)
 fun shareQuest(quest: Quest, context: Context) {
-    val connectionsClient = Nearby.getConnectionsClient(context)
-
-    val advertisingOptions = AdvertisingOptions.Builder()
-        .setStrategy(Strategy.P2P_STAR)
-        .build()
-
-    val payloadCallback = object : PayloadCallback() {
-        override fun onPayloadReceived(endpointId: String, payload: Payload) {
-            // Handle received payloads here if needed
-        }
-
-        override fun onPayloadTransferUpdate(endpointId: String, update: PayloadTransferUpdate) {
-            // Handle payload transfer updates if needed
-        }
-    }
-    Log.e("NFC", "Advertising")
-
-    try {
-        connectionsClient.startAdvertising(
-            "DeviceName", "com.example.geoquest", object : ConnectionLifecycleCallback() {
-                override fun onConnectionInitiated(endpointId: String, connectionInfo: ConnectionInfo) {
-                    // Automatically accept the connection on both sides.
-                    connectionsClient.acceptConnection(endpointId, payloadCallback)
-
-                    Log.e("NFC", "Accepted connection: $endpointId")
-                }
-
-                override fun onConnectionResult(endpointId: String, result: ConnectionResolution) {
-                    Log.e("NFC", "Connection result: $endpointId")
-                    if (result.status.isSuccess) {
-                        // We're connected! Can now start sending the quest data.
-                        Log.e("NFC", "Sending data")
-
-                        val questJson = convertQuestToJson(quest) // Assuming you have this method
-                        val bytesPayload = Payload.fromBytes(questJson.toByteArray())
-                        connectionsClient.sendPayload(endpointId, bytesPayload)
-
-                        Log.e("NFC", "Sent data")
-
-                    }
-                }
-
-                override fun onDisconnected(endpointId: String) {
-                    // We've been disconnected from this endpoint. No more data can be sent or received.
-                    Log.e("NFC", "Disconnected from endpoint: $endpointId")
-                }
-            },
-            advertisingOptions
-        )
-    } catch (e: Exception) {
-        Log.e("NFC", "Exception: $e")
-    }
+    startAdvertising(context)
 }
 
 
