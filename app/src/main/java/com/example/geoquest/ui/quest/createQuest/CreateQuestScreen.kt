7eddefaa6e45c0a3f0f8d209e8f2e4087aca8d6f
Sami Elmadani <sae53@uclive.ac.nz>
2023-10-06 18:25:30 +1300

    tooltip

--- app/src/main/java/com/example/geoquest/ui/quest/createQuest/CreateQuestScreen.kt
+++ app/src/main/java/com/example/geoquest/ui/quest/createQuest/CreateQuestScreen.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -15,23 +16,30 @@ import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Info
 import androidx.compose.material.icons.filled.Warning
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.PlainTooltipBox
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.BlendMode.Companion.Color
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -133,6 +141,9 @@ fun CreateQuestBody(
         }) {
             Text(text = stringResource(id = R.string.take_photo))
         }
+
+        PlainToolTipDemo()
+
         QuestInputForm(
             questDetails = questUiState.questDetails,
             onValueChange = onQuestValueChange,
@@ -184,6 +195,7 @@ fun QuestInputForm(
                 .widthIn(max = 280.dp)
 
         ) {
+
             OutlinedTextField(
                 value = questDetails.questTitle,
                 onValueChange = { onValueChange(questDetails.copy(questTitle = it)) },
@@ -314,4 +326,29 @@ fun CreateScreenPreview() {
             viewModel = viewModel(factory = AppViewModelProvider.Factory),
         )
     }
+}
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+private fun PlainToolTipDemo() {
+
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center,
+    ) {
+        PlainTooltipBox(tooltip = {
+            Text(
+                text = stringResource(id = R.string.tooltip_text),
+                textAlign = TextAlign.Center // Center-align the text
+            )
+        }) {
+            IconButton(onClick = { /**/ }, modifier = Modifier.tooltipAnchor()) {
+                Icon(
+                    imageVector = Icons.Filled.Info,
+                    contentDescription = null,
+                    tint = Color(android.graphics.Color.parseColor("#ADD8E6"))) // Replace with your hex code
+            }
+        }
+    }
+
+
 }
