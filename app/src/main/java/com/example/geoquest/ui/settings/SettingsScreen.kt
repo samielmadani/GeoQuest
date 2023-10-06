c822795625a94d3043d62a66871c020f6cd58090
Danish Khursheed Jahangir <dkj23@uclive.ac.nz>
2023-10-06 19:16:41 +1300

    Added validation to the username field in the settings page

--- app/src/main/java/com/example/geoquest/ui/settings/SettingsScreen.kt
+++ app/src/main/java/com/example/geoquest/ui/settings/SettingsScreen.kt
@@ -1,34 +1,35 @@
 package com.example.geoquest.ui.settings
 
-import android.Manifest
-import android.content.Context
-import android.content.pm.PackageManager
 import android.widget.Toast
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Warning
 import androidx.compose.material3.Button
-import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Switch
-import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
@@ -39,9 +40,6 @@ import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.core.app.ActivityCompat
-import androidx.core.app.NotificationCompat
-import androidx.core.app.NotificationManagerCompat
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.geoquest.GeoQuestTopBar
 import com.example.geoquest.R
@@ -81,7 +79,8 @@ fun SettingsScreen(
                 modifier = Modifier
                     .padding(contentPadding)
                     .fillMaxSize()
-                    .verticalScroll(scrollState, enabled = true),
+                    .verticalScroll(scrollState, enabled = true)
+                    .fillMaxWidth(),
                 horizontalAlignment = Alignment.CenterHorizontally,
             ) {
                 Text(
@@ -91,14 +90,53 @@ fun SettingsScreen(
                         fontSize = dimensionResource(id = R.dimen.text_size).value.sp
                     )
                 )
-                OutlinedTextField(
-                    value = viewModel.settingsState.userName,
-                    onValueChange = {
-                        viewModel.updateSettingsState(userName = it)
-                                    },
-                    label = { Text(stringResource(id = R.string.player_name)) },
-                    singleLine = true,
-                )
+                Box(
+                    modifier = Modifier
+                        .padding(dimensionResource(id = R.dimen.padding_medium))
+                        .widthIn(max = 280.dp),
+                    contentAlignment = Alignment.Center
+                ) {
+                    OutlinedTextField(
+                        value = viewModel.settingsState.userName,
+                        onValueChange = {
+                            viewModel.updateSettingsState(userName = it)
+                        },
+                        label = { Text(stringResource(id = R.string.player_name)) },
+                        singleLine = true,
+                        isError = !isValidText(viewModel.settingsState.userName) || viewModel.settingsState.userName.length > 24,
+                        supportingText = {
+                            if (viewModel.settingsState.userName.length > 24) {
+                                Text(
+                                    text = stringResource(
+                                        id = R.string.character_limit,
+                                        viewModel.settingsState.userName.length,
+                                        24
+                                    ),
+                                    modifier = Modifier.fillMaxWidth(),
+                                    color = MaterialTheme.colorScheme.error
+                                )
+                            } else if (!isValidText(viewModel.settingsState.userName)) {
+                                Text(
+                                    text = stringResource(id = R.string.validation_message),
+                                    modifier = Modifier.fillMaxWidth(),
+                                    color = MaterialTheme.colorScheme.error
+                                )
+                            }
+                        },
+                        trailingIcon = {
+                            if (viewModel.settingsState.userName.length > 24 || !isValidText(
+                                    viewModel.settingsState.userName
+                                )
+                            ) {
+                                Icon(
+                                    imageVector = Icons.Filled.Warning,
+                                    contentDescription = stringResource(id = R.string.validation_error),
+                                    tint = MaterialTheme.colorScheme.error
+                                )
+                            }
+                        },
+                    )
+                }
 
                 DeveloperOptionsToggle(
                     isChecked = viewModel.settingsState.developerOptions,
@@ -182,6 +220,11 @@ fun SettingsScreen(
                 }
             }
     }
+}
+
+@Composable
+fun isValidText(text: String): Boolean {
+    return text.isEmpty() || text.matches(Regex("(?=.*[a-zA-Z])[a-zA-Z0-9 ]+"))
 }
 
 @Composable
